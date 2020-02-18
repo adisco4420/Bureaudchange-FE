0c5087bf7ad7f4a6395cee491673b53da2bbb74d
alabi sodiq <adisco4420@gmail.com>
2020-02-18 23:12:18 +0100

    route main-layout to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { MainLayoutComponent } from './core/components/main-layout/main-layout.component';
+import { HomeComponent } from './core/components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainLayoutComponent, children: [
+    { path: '', component: HomeComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
