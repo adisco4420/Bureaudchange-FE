b8563d8cf64421f7b737f8d800a19fb72ae0f115
alabi sodiq <adisco4420@gmail.com>
2020-02-18 23:15:03 +0100

    navbar style logic on scroll functionality

--- src/app/core/components/main-header/main-header.component.ts
+++ src/app/core/components/main-header/main-header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare const $: any;
 @Component({
   selector: 'app-main-header',
   templateUrl: './main-header.component.html',
@@ -10,6 +10,16 @@ export class MainHeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.getNavLogic();
+  }
+  getNavLogic() {
+    const siteNav = $('#navbar');
+    siteNav.on('show.bs.collapse', function(e) {
+        $(this).parents('.nav-menu').addClass('menu-is-open');
+    });
+    siteNav.on('hide.bs.collapse', function(e) {
+        $(this).parents('.nav-menu').removeClass('menu-is-open');
+    });
   }
 
 }
