fd1fdb2af02d8ee941d6558814755676638fb3d8
alabi sodiq <adisco4420@gmail.com>
2020-02-27 21:36:22 +0100

    generate wallet component

---
+++ src/app/auth/components/wallet-setup/wallet-setup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-setup',
+  templateUrl: './wallet-setup.component.html',
+  styleUrls: ['./wallet-setup.component.css']
+})
+export class WalletSetupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
