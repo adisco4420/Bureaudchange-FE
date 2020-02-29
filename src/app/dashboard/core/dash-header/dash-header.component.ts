56b5d1bb2649af9a11bfe690cbdf1ea4529728b1
alabi sodiq <adisco4420@gmail.com>
2020-03-01 00:08:55 +0100

    update sidebar]

--- src/app/dashboard/core/dash-header/dash-header.component.ts
+++ src/app/dashboard/core/dash-header/dash-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const $: any;
 @Component({
   selector: 'app-dash-header',
   templateUrl: './dash-header.component.html',
@@ -10,6 +11,40 @@ export class DashHeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.jqueryFunction();
   }
+  
+
+  jqueryFunction() {
+        /*================================
+    sidebar collapsing
+    ==================================*/
+    $('.nav-btn').on('click', function() {
+      $('.page-container').toggleClass('sbar_collapsed');
+    });
+    /*================================
+    sidebar menu
+    ==================================*/
+    $('#menu').metisMenu();
+
+    /*================================
+    slimscroll activation
+    ==================================*/
+    $('.menu-inner').slimScroll({
+      height: 'auto'
+    });
+    $('.nofity-list').slimScroll({
+      height: '435px'
+    });
+    $('.timeline-area').slimScroll({
+      height: '500px'
+    });
+    $('.recent-activity').slimScroll({
+      height: 'calc(100vh - 114px)'
+    });
+    $('.settings-list').slimScroll({
+      height: 'calc(100vh - 158px)'
+    });
+}
 
 }
