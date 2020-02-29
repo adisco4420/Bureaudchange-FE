da506a5e94382d86d354ad411d5e0699461bf361
alabi sodiq <adisco4420@gmail.com>
2020-02-29 22:30:48 +0100

    dashboard layout updated

--- src/app/dashboard/dash-layout/dash-layout.component.ts
+++ src/app/dashboard/dash-layout/dash-layout.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dash-layout',
   templateUrl: './dash-layout.component.html',
-  styleUrls: ['./dash-layout.component.css']
+  styles: [`
+      @import "/assets/dashboard/css/metisMenu.css";
+      @import  '/assets/dashboard/css/font-awesome.min.css';
+      @import "/assets/dashboard/css/slicknav.min.css";
+      @Import "/assets/dashboard/css/typography.css";
+      @import "/assets/dashboard/css/default-css.css";
+      @import "/assets/dashboard/css/styles.css";
+      @import "/assets/dashboard/css/responsive.css";
+    `]
 })
 export class DashLayoutComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    console.log('dahboard');
   }
 
 }
