2cbed12addd320701a11cfe2d3ea87380f82bfbb
alabi sodiq <adisco4420@gmail.com>
2020-02-29 22:29:26 +0100

    route to dashboard layout

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashLayoutComponent } from './dash-layout/dash-layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: DashLayoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
