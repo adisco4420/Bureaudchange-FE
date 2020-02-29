e1e01f65c41232aeddef1c88477d33321a4c8592
alabi sodiq <adisco4420@gmail.com>
2020-02-29 21:35:57 +0100

    moved auth css to auth layout

--- src/app/auth/components/auth-layout.component.ts
+++ src/app/auth/components/auth-layout.component.ts
@@ -4,5 +4,6 @@ import { Component } from '@angular/core';
     selector: 'app-auth-layout',
     template: `<header class="bg-gradient p-75x" id="home"></header>
                 <div class="body"><router-outlet></router-outlet></div>`,
+    styles: [`@import "/assets/css/auth.css";`]
 })
  export class AuthLayoutComponent {}
