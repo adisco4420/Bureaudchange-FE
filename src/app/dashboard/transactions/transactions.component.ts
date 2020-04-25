6317cf22571078d06240fe891c6bc5cfdef3fbd9
Alabi Sodiq <adisco4420@gmail.com>
2020-04-25 15:43:31 +0100

    display transaction content

--- src/app/dashboard/transactions/transactions.component.ts
+++ src/app/dashboard/transactions/transactions.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { GeneralService } from 'src/app/shared/general.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { WalletService } from 'src/app/shared/wallet.service';
+import { TransI } from './../../models/trans.model';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import TransUtils from '../../utilts/trans.utilts';
+import CurrencyUtil from '../../utilts/currency.util';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.scss']
 })
-export class TransactionsComponent implements OnInit {
-
-  constructor() { }
+export class TransactionsComponent implements OnInit, OnDestroy {
+  private unscribe = new Subject();
+  transUtil = TransUtils;
+  cunbySymbol = CurrencyUtil.getCunBySymbol;
+  transList: TransI[];
+  constructor(
+    private walletSrv: WalletService,
+    private gs: GeneralService) { }
 
   ngOnInit() {
+    this.getTrans();
+  }
+
+  getTrans() {
+    this.walletSrv.fetchTrans().pipe(takeUntil(this.unscribe))
+      .subscribe(res => {
+        this.transList = this.gs.getSuccessData(res);
+        console.log(this.transList);
+      });
+  }
+  ngOnDestroy() {
+    this.unscribe.next();
+    this.unscribe.complete();
   }
 
 }
