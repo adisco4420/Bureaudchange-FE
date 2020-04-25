c4f2289b169aa64ac963c760ac9457f1428ffcb3
Alabi Sodiq <adisco4420@gmail.com>
2020-04-25 14:58:23 +0100

    recent transactions for landing page

--- src/app/dashboard/landing/landing.component.ts
+++ src/app/dashboard/landing/landing.component.ts
@@ -1,3 +1,5 @@
+import { TransI } from './../../models/trans.model';
+import { WalletService } from 'src/app/shared/wallet.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { UserWalletI } from 'src/app/auth/components/user.model';
@@ -6,6 +8,9 @@ import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
+import TransUtils from '../../utilts/trans.utilts';
+import CurrencyUtil from '../../utilts/currency.util';
+
 
 @Component({
   selector: 'app-landing',
@@ -13,15 +18,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit, OnDestroy {
+  transUtil = TransUtils;
+  cunbySymbol = CurrencyUtil.getCunBySymbol;
   unscribe = new Subject();
   walletList: UserWalletI[];
+  transList: TransI[];
   constructor(
     private authSrv: AuthService,
     public gs: GeneralService,
     private toastr: ToastrService,
+    private walletSrv: WalletService,
     private router: Router) {}
   ngOnInit() {
     this.getUserProfile();
+    this.getTrans();
   }
 
   getUserProfile() {
@@ -32,6 +42,12 @@ export class LandingComponent implements OnInit, OnDestroy {
         this.toastr.warning('Setup Wallet');
         this.router.navigate(['/auth/wallet-setup']);
       }
+    });
+  }
+  getTrans() {
+    this.walletSrv.fetchTrans().pipe(takeUntil(this.unscribe)).subscribe(res => {
+      const data = this.gs.getSuccessData(res);
+      this.transList = data.splice(0, 5);
     });
   }
   addCurrency() {
