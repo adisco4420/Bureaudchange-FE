41db55a322aad631d43c2cfbf475a58093fcc026
Alabi Sodiq <adisco4420@gmail.com>
2020-05-03 12:39:42 +0100

    get profile in dash header

--- src/app/dashboard/core/dash-header/dash-header.component.ts
+++ src/app/dashboard/core/dash-header/dash-header.component.ts
@@ -1,26 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { GeneralService } from 'src/app/shared/general.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { UserI } from 'src/app/auth/components/user.model';
 import { Router } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-dash-header',
   templateUrl: './dash-header.component.html',
   styleUrls: ['./dash-header.component.scss']
 })
-export class DashHeaderComponent implements OnInit {
+export class DashHeaderComponent implements OnInit, OnDestroy {
+  private unscribe = new Subject();
   user: UserI;
   constructor(
     private authSrv: AuthService,
-    private router: Router) {
+    private router: Router,
+    private gs: GeneralService) {
     this.authSrv.currentUserValue.subscribe(res => {
       this.user = res;
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getProfile();
+  }
+  getProfile() {
+    this.authSrv.userProfile().pipe(takeUntil(this.unscribe))
+      .subscribe(res => {
+        this.user = this.gs.getSuccessData(res);
+      });
+  }
   logout() {
     this.authSrv.logout();
     this.router.navigate(['/auth/login']);
   }
+
+  ngOnDestroy() {
+    this.unscribe.next();
+    this.unscribe.complete();
+  }
+
+
 }
